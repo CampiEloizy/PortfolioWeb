86fd3bcfde2fd7c7860b673713251b4935a28ca7
EloizyCampi <campieloizy80@gmail.com>
2022-06-22 23:25:32 -0400

    Finalizacao
    code

---
+++ javaS.js
@@ -0,0 +1,13 @@
+function mostracheio(imagem){
+    imagem.src="imagens/doce.jpg";
+}
+ 
+function mostravazio(imagem){
+ imagem.src="imagens/doce2.jpg";
+}
+
+function Aviso(){
+    Nome = String(document.formulario.nome.value);
+    Email = String(document. formulario.email);
+    alert ("Obrigada por se cadastrar " + Nome);
+}
